2a1e0c459c2cf1b0fdfdaac4f4fdd019c1e85c4d
Maksad Annamuradov <46725876+MaksadAnnamuradov@users.noreply.github.com>
2021-03-20 21:08:38 -0600

    Styled the main and the workout detial pages. So far working

--- JCMFitnessMobileApp/JCMFitnessMobileApp/Models/WorkOut.cs
+++ JCMFitnessMobileApp/JCMFitnessMobileApp/Models/WorkOut.cs
@@ -9,6 +9,7 @@ namespace JCMFitnessMobileApp.Models
         public DateTime CreatedDate { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Category { get; set; }
         public List<Exercise> Exercises { get; set; }
     }
 }
